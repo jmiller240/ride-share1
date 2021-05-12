c5884d6e3abcf7b68fe27986178171ddd5378154
Noah Gegner <noahgegner@macbook-pro.campus.tayloru.edu>
2021-05-11 22:02:53 -0400

    pdrivep

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -2,9 +2,9 @@
   <v-app-bar app dark color="#464343">
     <router-link v-bind:to="{ name: 'home-page' }">
       <img :src="'../../public/pdrivep.png'" height='40' />
-      <v-toolbar-title class="white--text">
-        Ride-Share
-      </v-toolbar-title>
+      <!--v-toolbar-title class="white--text">
+        pdrivep
+      </v-toolbar-title-->
     </router-link>
 
     <v-spacer></v-spacer>
