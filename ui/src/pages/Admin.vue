42b43394f5b94f715b243c283c99c641d9bdc817
jmiller240 <millerjd125@gmail.com>
2021-05-11 20:47:52 -0400

    merge

--- ui/src/pages/Admin.vue
+++ ui/src/pages/Admin.vue
@@ -33,7 +33,7 @@
             <v-dialog v-model="dialog" max-width="500px">
               <template v-slot:activator="{ on, attrs }">
                 <v-btn
-                  color="primary"
+                  color="secondary"
                   dark
                   class="mb-2"
                   v-bind="attrs"
